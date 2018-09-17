26aaadeecf7335a2cdefaab9b4197acf9eee498d
sysadmin29 <sysadmin29@cccdms.com>
2018-09-17 09:09:53 +0800

    added bsop, offshore, phil pages

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ $(window).scroll(function() {
         var scroll_top = $(window).scrollTop();
 
         if (scroll_top >= 200) {
-            $("#scrollNav").addClass("on-scroll");
+            $("#scrollNav").addClass("on-scroll text-light");
         } else {
-            $("#scrollNav").removeClass("on-scroll");
+            $("#scrollNav").removeClass("on-scroll text-light");
         }
                       
 });
